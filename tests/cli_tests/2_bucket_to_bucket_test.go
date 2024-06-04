fa793e6c67edc6ee4698e686714c97a1c8fb31cc
pewssh <pewssh@gmail.com>
2024-06-04 17:37:40 +0545

    updated commit

--- tests/cli_tests/2_bucket_to_bucket_test.go
+++ tests/cli_tests/2_bucket_to_bucket_test.go
@@ -1,6 +1,7 @@
 package cli_tests
 
 import (
+	"os"
 	"testing"
 	"time"
 	test "zs3server/tests/internal/cli/tests"
@@ -14,23 +15,32 @@ func TestZs3ServerBucket(testSetup *testing.T) {
 	// create two buckets for testing purpose
 
 	// test for moving the file from testbucket to testbucket2
-	t.RunSequentially("Test for moving file from testbucket to testbucket", func(t *test.SystemTest) {
+	t.RunSequentially("Test for moving file from testbucket to testbucket2", func(t *test.SystemTest) {
 		cli_utils.RunCommand(t, "mc mb zcn/testbucket", 1, time.Hour*2)
 
 		cli_utils.RunCommand(t, "mc mb zcn/testbucket2", 1, time.Hour*2)
 
-		cli_utils.RunCommand(t, "echo 'test' > a.txt", 0, time.Hour*2)
+		file, err := os.Create("a.txt")
+		if err != nil {
+			t.Fatalf("Error creating file: %v", err)
+		}
+		defer file.Close()
+
+		_, err = file.WriteString("test")
+		if err != nil {
+			t.Fatalf("Error writing to file: %v", err)
+		}
 
 		output_ls, _ := cli_utils.RunCommand(t, "mc mv a.txt zcn/testbucket", 1, time.Hour*2)
 
 		assert.NotContains(t, output_ls, "mc: <ERROR>")
-		// output, _ := cli_utils.RunCommand(t, "mc mv zcn/testbucket/a.txt  zcn/testbucket2 ", 1, time.Hour*2)
+		output, _ := cli_utils.RunCommand(t, "mc mv zcn/testbucket/a.txt  zcn/testbucket2 ", 1, time.Hour*2)
 
-		cli_utils.RunCommand(t, "mc rm zcn/testbucket2", 1, time.Hour*2)
+		cli_utils.RunCommand(t, "mc rb zcn/testbucket2 --force", 1, time.Hour*2)
 
-		cli_utils.RunCommand(t, "mc rm zcn/testbucket", 1, time.Hour*2)
+		cli_utils.RunCommand(t, "mc rb zcn/testbucket --force", 1, time.Hour*2)
 
-		// assert.NotContains(t, output, "mc: <ERROR>")
+		assert.NotContains(t, output, "mc: <ERROR>")
 
 	})
 
