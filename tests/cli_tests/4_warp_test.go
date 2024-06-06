c93d4014aaf1625c39d72943b04176195cc9c4da
pewssh <pewssh@gmail.com>
2024-06-06 17:30:53 +0545

    Added Test Cases for listing

---
+++ tests/cli_tests/4_warp_test.go
@@ -0,0 +1,54 @@
+package cli_tests
+
+import (
+	"log"
+	"os"
+	"strings"
+	"testing"
+	"time"
+	test "zs3server/tests/internal/cli/tests"
+	cliutils "zs3server/tests/internal/cli/util"
+)
+
+func appendToFile(filename string, data string) error {
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	if _, err := file.WriteString(data); err != nil {
+		return err
+	}
+	return nil
+}
+
+func TestZs3serverWarpTests(testSetup *testing.T) {
+	log.Println("Running Warp List Benchmark...")
+	t := test.NewSystemTest(testSetup)
+
+	output, err := cliutils.RunCommand(t, "./warp get --host=localhost:9000 --access-key=someminiouser --secret-key=someminiopassword --objects 1", 1, time.Hour*2)
+	if err != nil {
+		testSetup.Fatalf("Error running warp list: %v\nOutput: %s", err, output)
+	}
+	log.Println("Warp List Output:\n", output)
+	output_string := strings.Join(output, "\n")
+	output_string = strings.Split(output_string, "----------------------------------------")[1]
+
+	output_string = strings.Split(output_string, "warp: Starting cleanup")[0]
+
+	output_string = "Condition 1: objects: 10 \n--------\n" + output_string
+	err = appendToFile("warp-list_output.txt", output_string)
+
+	if err != nil {
+		testSetup.Fatalf("Error appending to file: %v\n", err)
+	}
+}
+
+func TestMain(m *testing.M) {
+	timeout := time.Duration(5 * time.Minute)
+	os.Setenv("GO_TEST_TIMEOUT", timeout.String())
+
+	os.Exit(m.Run())
+}
