06f68466664853bdec6cf72a11aa074fd1b5fc66
pewssh@gmail.com <pewssh@gmail.com>
2024-06-14 17:25:04 +0545

    updated

--- tests/cli_tests/4_warp_test.go
+++ tests/cli_tests/4_warp_test.go
@@ -27,10 +27,7 @@ func appendToFile(filename string, data string) error {
 	return nil
 }
 
-func TestZs3serverWarpTests(testSetup *testing.T) {
-	log.Println("Running Warp List Benchmark...")
-	t := test.NewSystemTest(testSetup)
-
+func read_file(testSetup *testing.T) (string, string, string, string) {
 	file, err := os.Open("hosts.yaml")
 	if err != nil {
 		testSetup.Fatalf("Error opening hosts.yaml file: %v\n", err)
@@ -49,7 +46,13 @@ func TestZs3serverWarpTests(testSetup *testing.T) {
 	port := hosts["port"].(int)
 	server := hosts["server"].(string)
 	host := strconv.FormatInt(int64(port), 10)
+	return server, host, accessKey, secretKey
 
+}
+func TestZs3serverWarpTests(testSetup *testing.T) {
+	log.Println("Running Warp List Benchmark...")
+	t := test.NewSystemTest(testSetup)
+	server, host, accessKey, secretKey := read_file(testSetup)
 	commandGenerated := "./warp get --host=" + server + ":" + host + " --access-key=" + accessKey + " --secret-key=" + secretKey + " --duration 30s" + " --obj.size 1KiB"
 	log.Println("Command Generated: ", commandGenerated)
 	output, err := cliutils.RunCommand(t, commandGenerated, 1, time.Hour*2)
@@ -70,93 +73,111 @@ func TestZs3serverWarpTests(testSetup *testing.T) {
 	}
 }
 
-// func TestZs3serverWarpConcurrentTests(testSetup *testing.T) {
-// 	log.Println("Running Warp List Benchmark with concurrent ...")
-// 	t := test.NewSystemTest(testSetup)
-// 	// ./zbox updateallocation --allocation $allocationId --size 999999999999
+func TestZs3serverWarpConcurrentTests(testSetup *testing.T) {
+	log.Println("Running Warp List Benchmark with concurrent ...")
+	t := test.NewSystemTest(testSetup)
+	server, host, accessKey, secretKey := read_file(testSetup)
 
-// 	output, err := cliutils.RunCommand(t, "./warp get --host=localhost:9000 --access-key=someminiouser --secret-key=someminiopassword --objects 50 --concurrent 50", 1, time.Hour*2)
-// 	if err != nil {
-// 		testSetup.Fatalf("Error running warp list: %v\nOutput: %s", err, output)
-// 	}
-// 	log.Println("Warp List Output:\n", output)
-// 	output_string := strings.Join(output, "\n")
-// 	output_string = strings.Split(output_string, "----------------------------------------")[1]
+	commandGenerated := "./warp get --host=" + server + ":" + host + " --access-key=" + accessKey + " --secret-key=" + secretKey + "  --concurrent 500 --duration 30s" + " --obj.size 1KiB"
+	log.Println("Command Generated: ", commandGenerated)
 
-// 	// output_string = strings.Split(output_string, "warp: Starting cleanup")[0]
+	output, err := cliutils.RunCommand(t, commandGenerated, 1, time.Hour*2)
 
-// 	output_string = "Condition 1: Get objects: 100 concurrent 50::  \n--------\n" + output_string
-// 	log.Println("APending to file with this stat ", output_string)
+	if err != nil {
+		testSetup.Fatalf("Error running warp list: %v\nOutput: %s", err, output)
+	}
+	log.Println("Warp List Output:\n", output)
+	output_string := strings.Join(output, "\n")
+	output_string = strings.Split(output_string, "----------------------------------------")[1]
 
-// 	err = appendToFile("warp-list_output.txt", output_string)
+	output_string = "Condition 1: Get objects: 100 concurrent 50::  \n--------\n" + output_string
+	log.Println("APending to file with this stat ", output_string)
 
-// 	if err != nil {
-// 		testSetup.Fatalf("Error appending to file: %v\n", err)
-// 	}
-// }
+	err = appendToFile("warp-list_output.txt", output_string)
 
-// func TestZs3serverPutWarpTests(testSetup *testing.T) {
-// 	log.Println("Running Warp Put Benchmark...")
-// 	t := test.NewSystemTest(testSetup)
+	if err != nil {
+		testSetup.Fatalf("Error appending to file: %v\n", err)
+	}
+}
 
-// 	output, err := cliutils.RunCommand(t, "./warp put --host=localhost:9000 --access-key=someminiouser --secret-key=someminiopassword", 1, time.Hour*2)
-// 	if err != nil {
-// 		testSetup.Fatalf("Error running warp put: %v\nOutput: %s", err, output)
-// 	}
-// 	log.Println("Warp Put Output:\n", output)
-// 	output_string := strings.Join(output, "\n")
-// 	output_string = strings.Split(output_string, "----------------------------------------")[1]
+func TestZs3serverPutWarpTests(testSetup *testing.T) {
+	log.Println("Running Warp Put Benchmark...")
+	t := test.NewSystemTest(testSetup)
 
-// 	output_string = "Condition 2 : Put  \n--------\n" + output_string
-// 	err = appendToFile("warp-put_output.txt", output_string)
+	server, host, accessKey, secretKey := read_file(testSetup)
 
-// 	if err != nil {
-// 		testSetup.Fatalf("Error appending to file: %v\n", err)
-// 	}
-// }
+	commandGenerated := "./warp put --host=" + server + ":" + host + " --access-key=" + accessKey + " --secret-key=" + secretKey + "  --concurrent 500 --duration 30s" + " --obj.size 1KiB"
+	log.Println("Command Generated: ", commandGenerated)
 
-// func TestZs3serverRetentionTests(testSetup *testing.T) {
-// 	log.Println("Running Warp Retention Benchmark...")
-// 	t := test.NewSystemTest(testSetup)
+	output, err := cliutils.RunCommand(t, commandGenerated, 1, time.Hour*2)
 
-// 	output, err := cliutils.RunCommand(t, "./warp retention --host=localhost:9000 --access-key=someminiouser --secret-key=someminiopassword --objects 1", 1, time.Hour*2)
-// 	if err != nil {
-// 		testSetup.Fatalf("Error running warp retention: %v\nOutput: %s", err, output)
-// 	}
-// 	log.Println("Warp Retention Output:\n", output)
-// 	output_string := strings.Join(output, "\n")
-// 	output_string = strings.Split(output_string, "----------------------------------------")[1]
-// 	output_string = strings.Split(output_string, "warp: Starting cleanup")[0]
+	if err != nil {
+		testSetup.Fatalf("Error running warp put: %v\nOutput: %s", err, output)
+	}
+	log.Println("Warp Put Output:\n", output)
+	output_string := strings.Join(output, "\n")
+	output_string = strings.Split(output_string, "----------------------------------------")[1]
 
-// 	output_string = "Condition 1: Retention : objects: 1 \n--------\n" + output_string
-// 	err = appendToFile("warp-put_output.txt", output_string)
+	output_string = "Condition 2 : Put  \n--------\n" + output_string
+	err = appendToFile("warp-put_output.txt", output_string)
 
-// 	if err != nil {
-// 		testSetup.Fatalf("Error appending to file: %v\n", err)
-// 	}
-// }
+	if err != nil {
+		testSetup.Fatalf("Error appending to file: %v\n", err)
+	}
+}
 
-// func TestZs3serverMultipartTests(testSetup *testing.T) {
-// 	log.Println("Running Warp Multipart Benchmark...")
-// 	t := test.NewSystemTest(testSetup)
+func TestZs3serverRetentionTests(testSetup *testing.T) {
+	log.Println("Running Warp Retention Benchmark...")
+	t := test.NewSystemTest(testSetup)
 
-// 	output, err := cliutils.RunCommand(t, "warp multipart --parts=500 --part.size=10MiB --host=localhost:9000 --access-key=someminiouser --secret-key=someminiopassword", 1, time.Hour*2)
+	server, host, accessKey, secretKey := read_file(testSetup)
 
-// 	if err != nil {
-// 		testSetup.Fatalf("Error running warp multipart: %v\nOutput: %s", err, output)
-// 	}
-// 	log.Println("Warp Multipart Output:\n", output)
-// 	output_string := strings.Join(output, "\n")
-// 	output_string = strings.Split(output_string, "----------------------------------------")[1]
-// 	output_string = strings.Split(output_string, "warp: Starting cleanup")[0]
+	commandGenerated := "./warp retention --host=" + server + ":" + host + " --access-key=" + accessKey + " --secret-key=" + secretKey + "  --concurrent 500 --duration 30s" + " --obj.size 1KiB"
+	log.Println("Command Generated: ", commandGenerated)
 
-// 	output_string = "Condition 1: Retention : objects: 1 \n--------\n" + output_string
-// 	err = appendToFile("warp-put_output.txt", output_string)
+	output, err := cliutils.RunCommand(t, commandGenerated, 1, time.Hour*2)
 
-// 	if err != nil {
-// 		testSetup.Fatalf("Error appending to file: %v\n", err)
-// 	}
-// }
+	if err != nil {
+		testSetup.Fatalf("Error running warp retention: %v\nOutput: %s", err, output)
+	}
+	log.Println("Warp Retention Output:\n", output)
+	output_string := strings.Join(output, "\n")
+	output_string = strings.Split(output_string, "----------------------------------------")[1]
+	output_string = strings.Split(output_string, "warp: Starting cleanup")[0]
+
+	output_string = "Condition 1: Retention : objects: 1 \n--------\n" + output_string
+	err = appendToFile("warp-put_output.txt", output_string)
+
+	if err != nil {
+		testSetup.Fatalf("Error appending to file: %v\n", err)
+	}
+}
+
+func TestZs3serverMultipartTests(testSetup *testing.T) {
+	log.Println("Running Warp Multipart Benchmark...")
+	t := test.NewSystemTest(testSetup)
+
+	server, host, accessKey, secretKey := read_file(testSetup)
+
+	commandGenerated := "./warp multipart --parts=500 --part.size=10MiB --host=" + server + ":" + host + " --access-key=" + accessKey + " --secret-key=" + secretKey + "  --concurrent 500 --duration 30s" + " --obj.size 1KiB"
+	log.Println("Command Generated: ", commandGenerated)
+
+	output, err := cliutils.RunCommand(t, commandGenerated, 1, time.Hour*2)
+	if err != nil {
+		testSetup.Fatalf("Error running warp multipart: %v\nOutput: %s", err, output)
+	}
+	log.Println("Warp Multipart Output:\n", output)
+	output_string := strings.Join(output, "\n")
+	output_string = strings.Split(output_string, "----------------------------------------")[1]
+	output_string = strings.Split(output_string, "warp: Starting cleanup")[0]
+
+	output_string = "Condition 1: Retention : objects: 1 \n--------\n" + output_string
+	err = appendToFile("warp-put_output.txt", output_string)
+
+	if err != nil {
+		testSetup.Fatalf("Error appending to file: %v\n", err)
+	}
+}
 
 func TestMain(m *testing.M) {
 	timeout := time.Duration(15 * time.Minute)
